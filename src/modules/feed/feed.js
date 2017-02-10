f6baf82cea3b9c55833ce9b35ba5e6f2fa8fb4a0
vuong <vuongduongbuh@gmail.com>
2017-02-11 01:02:22 +0700

    add done button

--- src/modules/feed/feed.js
+++ src/modules/feed/feed.js
@@ -1,9 +1,27 @@
+import { inject } from 'aurelia-dependency-injection';
+import { BindingEngine } from 'aurelia-binding';
+
+
+@inject(BindingEngine)
 export class Feed {
-    constructor() {
-        console.log("Feed");
+
+    constructor(bindingEngine) {
+        console.log(bindingEngine);
+        this.number = 250;
+        this.text = '';
+        this.bindingEngine = bindingEngine;
+        // subscribe
+        let subscription = bindingEngine.propertyObserver(this, 'text')
+            .subscribe((newValue, oldValue) => {
+                this.number = 250 - newValue.length;
+            });
     }
 
     compose() {
         console.log("compose");
+    }
+
+    textChanged(newVal, oldVal) {
+        console.log(newVal, oldVal);
     }
 }
