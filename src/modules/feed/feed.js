d71f788507de22bd94155040f3c561fbab2ce337
vuong <vuongduongbuh@gmail.com>
2017-02-28 00:55:56 +0700

    update connection

--- src/modules/feed/feed.js
+++ src/modules/feed/feed.js
@@ -23,60 +23,40 @@ export class Feed {
         this.assetsUrl = AppConstants.assetsUrl;
         this.isOpenConnectedFeed = [];
         this.newFeed = {};
-        this.number = 250;
+        this.newConnector = {};
+        this.newFeedLength = 250;
+        this.newConnectorLength = 250;
         this.bindingEngine = bindingEngine;
         // subscribe
-        let subscription = bindingEngine.propertyObserver(this.newFeed, 'rawText')
+        bindingEngine.propertyObserver(this.newFeed, 'rawText')
             .subscribe((newValue, oldValue) => {
-                this.number = 250 - newValue.length;
+                this.newFeedLength = 250 - newValue.length;
             });
+
+        bindingEngine.propertyObserver(this.newConnector, 'rawText')
+            .subscribe((newValue, oldValue) => {
+                this.newConnectorLength = 250 - newValue.length;
+            });
+
         this.getFeeds();
     }
 
     getFeeds() {
         this.appService.getFeeds()
             .then((feeds) => {
-                console.log(feeds);
                 this.feeds = feeds;
+                this.feeds = this.reArrangeFeedsWithConnections();
+                console.log(this.feeds);
+                //this.deleteAllFeeds();
                 return this.feeds;
             });
-
     }
 
     postNewFeed() {
         let laddaDoneBtn = Ladda.create(document.querySelector('.pl-btn--done'));
         laddaDoneBtn.start();
 
-        //Check if file is selected
-        if (this.selectedFiles) {
-            this.newFeed.file = this.selectedFiles[0];
-        }
-
-        //Convert rawText to hashTags & text
-        this.newFeed.text = this.newFeed.rawText;
-        if (HashTags(this.newFeed.rawText)) {
-            this.newFeed.hashTags = HashTags(this.newFeed.rawText);
-            _.forEach(this.newFeed.hashTags, (value, key) => {
-                this.newFeed.text = _.replace(this.newFeed.text, value, "");
-                this.newFeed.hashTags[key] = _.replace(value, "#", "");
-            });
-            this.newFeed.hashTags = _.join(this.newFeed.hashTags, " ");
-        }
-
-        let autoLinkerOptions = {
-            email: false,
-            phone: false
-        };
-
-        let autoLinker = new Autolinker(autoLinkerOptions);
-
-        let urlParser = autoLinker.parse(this.newFeed.rawText);
-
-        if (urlParser.length) {
-            this.newFeed.url = urlParser[0].url;
-            this.newFeed.text = _.replace(this.newFeed.text, this.newFeed.url, "");
-            console.log(this.newFeed.url);
-        }
+        this.newFeed = this.convertRawFeedToFeed(this.newFeed);
         //Add new feed
         this.appService.postNewFeed(this.newFeed)
             .then((feed) => {
@@ -89,13 +69,19 @@ export class Feed {
             });
     }
 
-    // showModalAddLink() {
-    //     this.dialogService.open({ viewModel: AddLinkModal, model: this.addedUrl }).then(response => {
-    //         if (!response.wasCancelled) {
-    //             this.newFeed.url = response.output;
-    //         }
-    //     });
-    // }
+    postNewConnector(feed, idx) {
+        this.newConnector = this.convertRawFeedToFeed(this.newConnector);
+        this.newConnector.connectTo = feed.id;
+        //Add new feed
+        this.appService.postNewFeed(this.newConnector)
+            .then((connectedFeed) => {
+                this.feeds[idx].isConnectionOpened = false;
+                this.feeds[idx].hasConnection = true;
+                this.newConnector = {};
+                this.feeds.splice(idx + 1, 0, connectedFeed);
+            }, () => {
+            });
+    }
 
     showModalConfirmDelete(id, idx) {
         this.dialogService.open({ viewModel: ConfirmDeleteModal }).then(response => {
@@ -110,16 +96,90 @@ export class Feed {
             .then((success) => {
                 this.feeds.splice(idx, 1);
                 console.log(success);
-            })
+            });
     }
 
     removeSelectedFile() {
-        this.selectedFiles = null;
+        this.newFeed.selectedFiles = null;
         this.isFileSelected = false;
     }
 
     triggerClickInputFiles() {
         let input = document.querySelector(".pl-input--files");
         input.click();
+    }
+
+    convertRawFeedToFeed(rawFeed) {
+        let feed = rawFeed;
+
+        //Check if file is selected
+        if (feed.selectedFiles) {
+            feed.file = feed.selectedFiles[0];
+        }
+
+        //Convert rawText to hashTags & text
+        feed.text = feed.rawText;
+        if (HashTags(feed.rawText)) {
+            feed.hashTags = HashTags(feed.rawText);
+            _.forEach(feed.hashTags, (value, key) => {
+                feed.text = _.replace(feed.text, value, "");
+                feed.hashTags[key] = _.replace(value, "#", "");
+            });
+            feed.hashTags = _.join(feed.hashTags, " ");
+        }
+
+        let autoLinkerOptions = {
+            email: false,
+            phone: false
+        };
+
+        let autoLinker = new Autolinker(autoLinkerOptions);
+        let urlParser = autoLinker.parse(feed.rawText);
+
+        if (urlParser.length) {
+            feed.url = urlParser[0].url;
+            feed.text = _.replace(feed.text, feed.url, "");
+        }
+
+        return feed;
+    }
+
+    openConnectionToAFeed(feed) {
+        _.forEach(this.feeds, (value, key) => {
+            this.feeds[key].isConnectionOpened = false;
+        });
+
+        feed.isConnectionOpened = true;
+        this.isInputOnFocus = false;
+    }
+
+    reArrangeFeedsWithConnections() {
+        let resultFeeds = _.filter(this.feeds, (value) => {
+            return !value.connectTo;
+        });
+
+        let connectorFeeds = _.filter(this.feeds, (value) => {
+            return value.connectTo;
+        });
+
+        _.forEach(resultFeeds, (result, resultKey) => {
+            _.forEachRight(connectorFeeds, (connector, conectorKey) => {
+                if (connector.connectTo === result.id) {
+                    result.hasConnection = true;
+                    resultFeeds.splice(resultKey + 1, 0, connector);
+                }
+            });
+        });
+
+        return resultFeeds;
+    }
+
+    deleteAllFeeds() {
+        _.forEach(this.feeds, (value, key) => {
+            this.appService.deleteFeed(value.id)
+                .then((success) => {
+                    console.log(success);
+                })
+        })
     }
 }
