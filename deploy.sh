de6d171cd7d8be6db20362bee30d6677a4a1b688
Jonas Wittlin <jw@headbits.com>
2017-07-05 11:26:25 +0200

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,10 @@ echo
 
 au build --env prod
 
+mkdir dist
+mv scripts dist/scripts
+cp index.html dist/
+
 rsync -az --progress -e "ssh -p22" ./dist/* $USER@$HOST:$FOLDER
+
+rm -r dist
--- src/app-constant.js
+++ src/app-constant.js
@@ -3,7 +3,7 @@ export const AppConstants = {
   auth0Domain: "hbconsulting.eu.auth0.com",
   auth0ClientId: "Dk1L_G1OxTmhAuT0CMF0BByHQoPfZYKi",
   baseUrl: "https://api.ankker.headbits.net/api",
-  assetsUrl: "https://api.ankker.headbits.net/static"
+  assetsUrl: "https://api.ankker.headbits.net"
   //baseUrl: "http://localhost:7869/api",
   //assetsUrl: "http://localhost:7869"
 };
